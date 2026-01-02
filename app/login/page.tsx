52daf788f2667e001ce1c190b35b27afb09a7483
SquadAssisstant <metalgrimm21@gmail.com>
2026-01-01 16:54:56 -0800

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,7 @@
-export const dynamic = "force-dynamic";
 "use client";
+
+export const dynamic = "force-dynamic";
+
 
 import { Suspense, useEffect, useState } from "react";
 import Link from "next/link";
